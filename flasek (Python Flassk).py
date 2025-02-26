7c7f67663ceffd718906f86ac5c8c92320ad1c9d
YazKii <yahyamuzaki412@gmail.com>
2025-02-26 21:46:59 +0700

    flasek (Python Flassk).py

--- flasek (Python Flassk).py
+++ flasek (Python Flassk).py
@@ -32,5 +32,4 @@ def save_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5003)
-
+    app.run(host="0.0.0.0", debug=True, port=5002)
